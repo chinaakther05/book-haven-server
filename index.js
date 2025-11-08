8dc5496b6957d8d52f095e4dd3f58a468326e277
Your Namesohaggaji07@gmail.com <sohaggaji07@gmail.com>
2025-11-09 03:10:28 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require('express')
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+const uri = "mongodb+srv://bookHavendb:F75V8huQGxxB75H4@cluster0.qoielcp.mongodb.net/?appName=Cluster0";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+
+app.get('/', (req, res) =>{
+    res.send('Books server is running')
+})
+
+async function run (){
+    try{
+      await client.connect();
+
+     const db = client.db('BookHavendb');
+     const booksCollection = db.collection("books");
+     
+     // create a book 
+     app.post('/books', async (req, res)=>{
+        const newBook = req.body;
+        const result = await booksCollection.insertOne(newBook);
+        res.send(result);
+     })
+
+     // delete book
+     
+  
+
+      await client.db("admin").command({ ping: 1 });
+       console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    }
+    finally{
+
+    }
+}
+
+run().catch(console.dir)
+
+app.listen(port, () =>{
+    console.log(`Books server is running on port: ${port}`)
+})
